89a858745c5757684ace7cb8aa422c29f07979c8
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2025-07-13 13:51:29 +0000

    taiss

--- setup-server.sh
+++ setup-server.sh
@@ -73,7 +73,7 @@ sudo ufw allow 80
 sudo ufw allow 443
 sudo ufw --force enable
 
-echo "✅ Configuration du serveur terminée!"
+echo "✅ Configuration du serveur terminée! c est bon"
 echo "📝 Prochaines étapes:"
 echo "1. Configurez les secrets GitHub Actions"
 echo "2. Configurez le webhook GitHub"
